b3683784dec5040b35d1a37ddfbe37b025f02237
devunmistry <mistry.devun@gmail.com>
2022-01-04 12:35:54 +0000

    Refactor: router_a
    create pytest fixture for router_a, instead of creating object in each test

--- test_sky_project.py
+++ test_sky_project.py
@@ -1,13 +1,20 @@
 from unittest import mock
+import pytest
 
 from sky_project import Router
 
-def test_Router_createsRouterObject_whenRouterObjectInstantiated():
+@pytest.fixture
+def router_a():
+    router_a = Router()
+    return router_a
+
+
+def test_Router_createsRouterObject_whenRouterObjectInstantiated(router_a):
     router = Router()
     result = isinstance(router, Router)
     assert result == True
 
-def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalled2():
+def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalled2(router_a):
     with mock.patch("sky_project.manager") as mocked__manager: #at instance where manager called, replaced with mock
         router = Router()
         router.configure_loopback()
