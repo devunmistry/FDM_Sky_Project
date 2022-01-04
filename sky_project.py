d730058fb6bd7108644236aac2b01dc39f428c86
devunmistry <mistry.devun@gmail.com>
2022-01-04 12:47:31 +0000

    Exception for invalid ip address and/or port

--- sky_project.py
+++ sky_project.py
@@ -1,4 +1,5 @@
 from ncclient import manager
+from ncclient.transport.errors import SSHError
 
 class Router():
     def configure_loopback(self, host, port, username, password):
@@ -8,11 +9,14 @@ class Router():
         :return: None
         '''
 
-        with manager.connect_ssh(
-            host = host,
-            port = port,
-            username = username,
-            password = password,
-            hostkey_verify=False,
-            device_params={"name":"csr"}) as m:
-            pass
+        try:
+            with manager.connect_ssh(
+                host = host,
+                port = port,
+                username = username,
+                password = password,
+                hostkey_verify=False,
+                device_params={"name":"csr"}) as m:
+                pass
+        except (SSHError):
+            raise SSHError("Invalid IP address and/or port number")
--- test_sky_project.py
+++ test_sky_project.py
@@ -1,37 +1,49 @@
-from unittest import mock
-import pytest
+from unittest import mock, TestCase
+
+from ncclient.transport.errors import SSHError
 
 from sky_project import Router
 
-@pytest.fixture
-def router_a():
-    router_a = Router()
-    return router_a
+class TestConfigureLoopback(TestCase):
+    def setUp(self):
+        self.router = Router()
 
-def test_Router_createsRouterObject_whenRouterObjectInstantiated(router_a):
-    result = isinstance(router_a, Router)
-    assert result == True
+    def test_Router_createsRouterObject_whenRouterObjectInstantiated(self):
+        result = isinstance(self.router, Router)
+        assert result == True
 
-def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalledRouterA(router_a):
-    with mock.patch("sky_project.manager") as mocked_manager: #at instance where sky_project.manager called, replaced with mock_manager
-        router_a.configure_loopback("192.168.0.101", "830", "cisco", "cisco")
-        result = mocked_manager.connect_ssh.assert_called_once_with(
-            host='192.168.0.101',
-            port='830',
-            username='cisco',
-            password='cisco',
-            hostkey_verify=False,
-            device_params={'name': 'csr'}) #asserts if mocked_manager.connect_ssh called with correct arguements
-        assert result == None
+    def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalledRouter_101(self):
+        with mock.patch("sky_project.manager") as mocked_manager: #at instance where sky_project.manager called, replaced with mock_manager
+            self.router.configure_loopback("192.168.0.101", "830", "cisco", "cisco")
+            result = mocked_manager.connect_ssh.assert_called_once_with(
+                host='192.168.0.101',
+                port='830',
+                username='cisco',
+                password='cisco',
+                hostkey_verify=False,
+                device_params={'name': 'csr'}) #asserts if mocked_manager.connect_ssh called with correct arguements
+            assert result == None
 
-def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalledRouterB():
-    with mock.patch("sky_project.manager") as mocked_manager:
-        router_b = Router()
-        router_b.configure_loopback("192.168.0.102", "830", "router", "router")
-        mocked_manager.connect_ssh.assert_called_once_with(
-            host='192.168.0.102',
-            port='830',
-            username='router',
-            password='router',
-            hostkey_verify=False,
-            device_params={'name': 'csr'})
+    def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalledRouter_102(self):
+        with mock.patch("sky_project.manager") as mocked_manager:
+            self.router.configure_loopback("192.168.0.102", "830", "router", "router")
+            mocked_manager.connect_ssh.assert_called_once_with(
+                host='192.168.0.102',
+                port='830',
+                username='router',
+                password='router',
+                hostkey_verify=False,
+                device_params={'name': 'csr'})
+
+    def test_configureLoopback_handlesException_whenConfigureLoopbackCalledWithInvalidIPAddressOrPort(self):
+        self.assertRaisesRegex(
+            SSHError,
+            "Invalid IP address and/or port number",
+            self.router.configure_loopback,
+            "192.168.0.100", "830", "cisco", "cisco")
+
+        self.assertRaisesRegex(
+            SSHError,
+            "Invalid IP address and/or port number",
+            self.router.configure_loopback,
+            "192.168.0.101", "800", "cisco", "cisco")
