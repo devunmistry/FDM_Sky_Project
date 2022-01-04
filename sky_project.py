5f4b68142e9e1c65443565ebb55e5b45479666a8
devunmistry <mistry.devun@gmail.com>
2022-01-04 12:32:46 +0000

    Verify configure_loopback() calls ncclient.manager.connect()
    In order to configure a loopback interface for a router, the function first needs to establish a connection - this tests that the connection has been made with the correct variables

--- sky_project.py
+++ sky_project.py
@@ -1,2 +1,18 @@
+from ncclient import manager
+
 class Router():
-    pass
+    def configure_loopback(self):
+        '''
+        Configures a given loopback interface using given parameters
+        :param self: self
+        :return: None
+        '''
+
+        with manager.connect_ssh(
+            host = "192.168.0.101",
+            port = "830",
+            username = "cisco",
+            password = "cisco",
+            hostkey_verify=False,
+            device_params={"name":"csr"}) as m:
+            print("connected")
--- test_sky_project.py
+++ test_sky_project.py
@@ -1,7 +1,20 @@
-import pytest
+from unittest import mock
+
 from sky_project import Router
 
 def test_Router_createsRouterObject_whenRouterObjectInstantiated():
     router = Router()
     result = isinstance(router, Router)
     assert result == True
+
+def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalled2():
+    with mock.patch("sky_project.manager") as mocked__manager: #at instance where manager called, replaced with mock
+        router = Router()
+        router.configure_loopback()
+        result = mocked__manager.connect_ssh.assert_called_once_with(
+            host='192.168.0.101',
+            port='830', username='cisco',
+            password='cisco',
+            hostkey_verify=False,
+            device_params={'name': 'csr'}) #asserts if mocked manager.connect_ssh called with correct arguements
+        assert result == None
