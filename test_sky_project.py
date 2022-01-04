91d8e0a8501acd8d67f7a35bc71c8590fb81cb55
devunmistry <mistry.devun@gmail.com>
2022-01-04 12:39:04 +0000

    Refactor: router_a
    create pytest fixture for router_a, instead of creating object in each test

--- test_sky_project.py
+++ test_sky_project.py
@@ -10,18 +10,16 @@ def router_a():
 
 
 def test_Router_createsRouterObject_whenRouterObjectInstantiated(router_a):
-    router = Router()
-    result = isinstance(router, Router)
+    result = isinstance(router_a, Router)
     assert result == True
 
-def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalled2(router_a):
-    with mock.patch("sky_project.manager") as mocked__manager: #at instance where manager called, replaced with mock
-        router = Router()
-        router.configure_loopback()
-        result = mocked__manager.connect_ssh.assert_called_once_with(
+def test_configureLoopback_callsNcclientManagerConnectssh_whenConfigureLoopbackCalled(router_a):
+    with mock.patch("sky_project.manager") as mocked_manager: #at instance where manager called, replaced with mock_manager
+        router_a.configure_loopback()
+        result = mocked_manager.connect_ssh.assert_called_once_with(
             host='192.168.0.101',
             port='830', username='cisco',
             password='cisco',
             hostkey_verify=False,
-            device_params={'name': 'csr'}) #asserts if mocked manager.connect_ssh called with correct arguements
+            device_params={'name': 'csr'}) #asserts if mocked_manager.connect_ssh called with correct arguements
         assert result == None
