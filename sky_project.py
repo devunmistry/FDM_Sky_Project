d6f8cab848c40d4717a61b8ba562d395a87a0e93
devunmistry <mistry.devun@gmail.com>
2022-01-04 14:30:45 +0000

    Refactor - move ncclient.manager.connect_ssh to decorator

--- sky_project.py
+++ sky_project.py
@@ -1,37 +1,74 @@
 from ipaddress import ip_address
 from ncclient import manager
 from ncclient.transport.errors import AuthenticationError, SSHError
-from ncclient.operations.rpc import RPCError
+from ncclient.operations.rpc import RPCError 
+from functools import wraps
 
 from xml_functions.xml_function_configure_loopback import configure_loopback_xml_renderer
 
 class Router():
+
+    def __init__(self, host, port, username, password):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+
+    def _connect_ssh_decorator(func):
+        '''
+        Decorator. Opens ssh connection with router. Wrapper function is connect_ssh_decorator_wrapper
+        :param func: function to be wrapped within decorator
+        :return: connect_ssh_decorator_wrapper
+        '''
+
+        @wraps(func)
+        def connect_ssh_decorator_wrapper(self, *args):
+            '''
+            Decorator wrapper. Calls ncclient.manager.connect_ssh to open ssh connection with router
+            :param self: self
+            :param *args: arguements to be passed to wrapped function
+            :return: none 
+            '''
+
+            #check router socket ip address is of valid format
+            try:
+                ip_address(self.host)
+            except(ValueError) as e:
+                print("%s: Invalid ip address for router socket" % (e.__class__))
+                return
+
+            #check router socket port is of valid format
+            if type(self.port) is not int or self.port < 1 or self.port > 65535:
+                print("Invalid port for router socket")
+                return
+
+            try:
+                with manager.connect_ssh(
+                    host = self.host,
+                    port = self.port,
+                    username = self.username,
+                    password = self.password,
+                    hostkey_verify = False,
+                    device_params = {"name":"csr"}) as m:
+                        return func(self, m, *args)
+            except (SSHError) as e:
+                print ("%s: Could not open router socket %s:%s - could be incorrect ip address and/or port number" % (e.__class__, self.host, self.port))
+            except (AuthenticationError) as e:
+                print("%s: Incorrect router SSH username and/or password" % (e.__class__))
+
+        return connect_ssh_decorator_wrapper
     
-    def configure_loopback(self, host, port, username, password, loopback_id, loopback_ip, loopback_subnet_mask):
+    @_connect_ssh_decorator
+    def configure_loopback(self, m, loopback_id, loopback_ip, loopback_subnet_mask):
         '''
         Configures a given loopback interface using given parameters
         :param self: self
-        :param host: ip address for router
-        :param port: port number for router ssh connection - normally 830, though have not set as default value
-        :param username: username for router ssh login - privilege should have been set to 15 to allow necessary ssh access 
-        :param password: password for router ssh login
+        :param m: open connection, passed from _connect_ssh_decorator
         :param loopback_id: id number for the loopback being configured
         :param loopback_ip: ip address for the loopback being configured
         :param loopback_subnet_mask: subnet mask for the loopback being configured
         :return: None
         '''
-
-        #check router socket ip address is of valid format
-        try:
-            ip_address(host)
-        except(ValueError) as e:
-            print("%s: Invalid ip address for router socket" % (e.__class__))
-            return
-
-        #check router socket port is of valid format
-        if type(port) is not int or port < 1 or port > 65535:
-            print("Invalid port for router socket")
-            return
 
         #check loopback id is of valid format
         if type(loopback_id) is not int or loopback_id < 0 or loopback_id > 2147483647:
@@ -45,30 +82,19 @@ class Router():
             print("%s: Invalid ip address for loopback interface" % (e.__class__))
             return
 
+        #check loopback subnet mask is of valid format
         try:
-            with manager.connect_ssh(
-                host = host,
-                port = port,
-                username = username,
-                password = password,
-                hostkey_verify=False,
-                device_params={"name":"csr"}) as m:
-                    conf = configure_loopback_xml_renderer(loopback_id, loopback_ip, loopback_subnet_mask)
-                    m.edit_config(target = "running", config = conf, default_operation="merge")
+            ip_address(loopback_subnet_mask)
+        except(ValueError) as e:
+            print("%s: Invalid subnet mask for loopback interface" % (e.__class__))
+            return
 
-        except (SSHError) as e:
-            print ("%s: Could not open router socket %s:%s - could be incorrect ip address and/or port number" % (e.__class__, host, port))
-        except (AuthenticationError) as e:
-            print("%s: Incorrect router SSH username and/or password" % (e.__class__))
+        try:
+            conf = configure_loopback_xml_renderer(loopback_id, loopback_ip, loopback_subnet_mask)
+            m.edit_config(target = "running", config = conf, default_operation="merge")
         except (RPCError) as e:
             print("%s: Loopback interface configuration error - various possible causes, including unavailable ip address or invalid subnet mask" % (e.__class__))
-    
-    def delete_loopback(self):
-        with manager.connect_ssh(
-            host = "192.168.0.101",
-            port = 830,
-            username = "cisco",
-            password = "cisco",
-            hostkey_verify=False,
-            device_params={"name":"csr"}) as m:
-                pass
+
+    @_connect_ssh_decorator
+    def delete_loopback(self, m):
+        pass
